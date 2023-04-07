902ffca8431580051cb5b46b2608fc6b6279d295
yohn-maistre <josejr2498@gmail.com>
2023-04-08 08:45:05 +0900

    model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -77,7 +77,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop='always'
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
