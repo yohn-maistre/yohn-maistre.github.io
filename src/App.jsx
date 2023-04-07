1f6f3aa9fdd910b89f5fe1ff22aff15bb2afc436
yohn-maistre <josejr2498@gmail.com>
2023-04-08 03:40:01 +0900

    model

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,8 @@ const App = () => {
         <Hero />
       </div>
       <About />
-      {/* <Experience /> */}
       <Tech />
       <Works />
-      {/* <Feedbacks /> */}
       <div className='relative z-0'>
         <Contact />
         <StarsCanvas />
